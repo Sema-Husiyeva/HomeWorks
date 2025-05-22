4121446809cf9b131c7e96530f45bfbe78421003
Sema Husiyeva <semahusiyeva64@gmail.com>
2025-05-23 03:16:18 +0400

    edit help and faq page

--- Final-Project-Budget/src/components/UI/Slider/slider.tsx
+++ Final-Project-Budget/src/components/UI/Slider/slider.tsx
@@ -1,58 +1,65 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation } from 'swiper/modules';
-import activityIcon from '../../../assets/svg/home-slider-activity.svg';
-import walletIcon from '../../../assets/svg/home-slider-wallet.svg';
-import paperIcon from '../../../assets/svg/home-slider-paper.svg';
-import cardIcon from '../../../assets/svg/home-slider-card.svg';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import './slider.scss';
+import classNames from 'classnames';
 
 
-const Slider = () => {
-    const items = [
-    { icon: activityIcon, text: "Simple interface/app that makes it easy to control your cash flows" },
-    { icon: walletIcon, text: "Fast transaction entry - Add all transactions with one click" },
-    { icon: paperIcon, text: "Creating templates to simplify and speed up the entry of transactions" },
-    { icon: cardIcon, text: "Ability to control expenses of different accounts from one source" },
-    { icon: activityIcon, text: "Simple interface that makes it easy to control your cash flows" },
-    { icon: walletIcon, text: "Fast transaction entry. Add all transactions with one click" },
-    { icon: paperIcon, text: "Creating templates to simplify and speed up the entry of transactions!" },
-    { icon: cardIcon, text: "Ability to control expenses of different accounts from one source." },
-  ];
+interface ISliderProps {
+  items: { id:number; icon: string; title?: string; subtitle?: string; text: string; avatar?:string; name?: string }[];
+  title?: string;
+  stars?: string;
+  className?: string;
+  variant1?: string;
+  variant2?: string;
+}
 
+const Slider: React.FC<ISliderProps> = ({ items, title, variant1, variant2, className }) => {
   return (
-     <div className="slider-section">
+    <section className="slider-section">
       <div className="slider-section-header">
-       <h2>What the app does</h2>
+        <h2>{title}</h2>
 
-       <div className="slider-section-header-buttons">
-        <button className="swiper-button-prev filled"></button>
-        <button className="swiper-button-next filled"></button>
-       </div>
-     </div>
+        <div className="slider-section-header-buttons">
+          <button className="swiper-button-prev filled"></button>
+          <button className="swiper-button-next filled"></button>
+        </div>
+      </div>
 
-     <Swiper
+      <Swiper
         modules={[Navigation]}
         navigation={{
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
         }}
         slidesPerView={3}
         spaceBetween={30}
         className="mySwiper"
       >
-      {items.map((item) => (
-        <SwiperSlide key={item.text}>
-          <div className="slider-section-card">
-          <img src={item.icon} alt='icon' />
-          <p className="text">{item.text}</p>
-        </div>
-        </SwiperSlide>
-       ))}
-     </Swiper>
-     </div>
+        {items.map((item) => (
+          <SwiperSlide key={item.id}>
+            <div className={classNames('slider-section-card', className, {
+                        'slider-section-card-blue-bg': variant1 === 'blue',
+                        'slider-section-card-none-bg': variant1 === 'none',
+                        'slider-section-card-white-bg': variant1 === 'white',
+                  })}>
+              <img className={classNames('slider-section-card-img', className, {
+                'slider-section-card-img-star': variant2 === 'star',
+              })} src={item.icon} alt="icon" />
+              {item.title && <h3>{item.title}</h3>}
+              <p className="text">{item.text}</p>
+
+              <div className='slider-section-card-user'>
+                {item.avatar && <img src={item.avatar} alt="avatar" />}
+                {item.name && <h3>{item.name}</h3>}
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </section>
   );
 };
 
-export default Slider
+export default Slider;
--- Final-Project-Budget/src/components/pages/Home/home.tsx
+++ Final-Project-Budget/src/components/pages/Home/home.tsx
@@ -9,10 +9,24 @@ import startWalletIcon from '../../../assets/svg/home-start-wallet.svg';
 import startPaperIcon from '../../../assets/svg/home-start-paper.svg';
 import startLineIcon from '../../../assets/svg/home-start-line.svg';
 import budgetVideo from '../../../assets/videos/budget-video.mp4';
+import activityIcon from '../../../assets/svg/home-slider-activity.svg';
+import walletIcon from '../../../assets/svg/home-slider-wallet.svg';
+import paperIcon from '../../../assets/svg/home-slider-paper.svg';
+import cardIcon from '../../../assets/svg/home-slider-card.svg';
 import './home.scss';
 
 
 const Home = () => {
+   const items = [
+    { id:1, icon: activityIcon, text: "Simple interface/app that makes it easy to control your cash flows" },
+    { id:2, icon: walletIcon, text: "Fast transaction entry - Add all transactions with one click" },
+    { id:3, icon: paperIcon, text: "Creating templates to simplify and speed up the entry of transactions" },
+    { id:4, icon: cardIcon, text: "Ability to control expenses of different accounts from one source" },
+    { id:5, icon: activityIcon, text: "Simple interface that makes it easy to control your cash flows" },
+    { id:6, icon: walletIcon, text: "Fast transaction entry. Add all transactions with one click" },
+    { id:7, icon: paperIcon, text: "Creating templates to simplify and speed up the entry of transactions!" },
+    { id:8, icon: cardIcon, text: "Ability to control expenses of different accounts from one source." },
+    ];
    const navigate = useNavigate();
     const [isMonthly, setIsMonthly] = useState(true);
 
@@ -21,10 +35,16 @@ const Home = () => {
     };
   return (
     <section className='home-section'>
-      <Banner title='Slogan' description='Take control of your finances with ease. Track your spending, set smart goals, and save more every day. Budgeting has never been this simple.' image={bannerImg}/>
+
+      <Banner
+        title='Slogan'
+        description='Take control of your finances with ease. Track your spending, set smart goals, and save more every day. Budgeting has never been this simple.'
+        image={bannerImg}
+      />
 
       <div className='home-section-start'>
         <h1 className='home-section-start-title'>How to start</h1>
+
         <div className='home-section-start-icons'>
           <div><img src={startActivityIcon} alt="start-activity-icon" /></div>
           <img src={startLineIcon} alt="start-line-icon" />
@@ -32,17 +52,18 @@ const Home = () => {
           <img src={startLineIcon} alt="start-line-icon" />
           <div><img src={startPaperIcon} alt="start-paper-icon" /></div>
         </div>
+
         <div className='home-section-start-text'>
           <p>Register with email or phone number.</p>
           <p>He creates his initial account to record his transactions.</p>
           <p>You can add transactions and track your account.</p>
         </div>
 
-       <div className='home-section-start-video'>
-        <video src={budgetVideo} controls autoPlay muted loop />
-       </div>
+        <div className='home-section-start-video'>
+          <video src={budgetVideo} controls autoPlay muted loop />
+        </div>
 
-        <Slider/>
+        <Slider items={items} title='What the app does' variant1='blue' />
       </div>
 
       <section className='home-section-subscription'>
@@ -52,34 +73,35 @@ const Home = () => {
         <div className='home-section-subscription-switch'>
           <p className='home-section-subscription-switch-name'>Yearly</p>
           <label>
-           <input type="checkbox" checked={isMonthly} onChange={handleToggle} />
-             <span className='slider'></span>
-           </label>
-           <p className='home-section-subscription-switch-name'>Monthly</p>
+            <input type="checkbox" checked={isMonthly} onChange={handleToggle} />
+            <span className='slider'></span>
+          </label>
+          <p className='home-section-subscription-switch-name'>Monthly</p>
         </div>
 
         <div className='home-section-subscription-cards'>
-            <div className='home-section-subscription-card left'>
-              <h2>Basic plan</h2>
-              <h1>{isMonthly ? "$3/Month" : "$30/Year"}</h1>
-              <p>Access to basic features</p>
-              <p>Monthly budget tracking</p>
-              <p>Cancel anytime</p>
-              <Button text="Get Started" onClick={() => navigate('/payment') } className='subscription-banner-section-card-btn' variant="blue"/>
-            </div>    
+          <div className='home-section-subscription-card left'>
+            <h2>Basic plan</h2>
+            <h1>{isMonthly ? "$3/Month" : "$30/Year"}</h1>
+            <p>Access to basic features</p>
+            <p>Monthly budget tracking</p>
+            <p>Cancel anytime</p>
+            <Button text="Get Started" onClick={() => navigate('/payment')} className='subscription-banner-section-card-btn' variant="blue" />
+          </div>
 
-            <div className='home-section-subscription-card right'>
-              <h2>Premium plan</h2>
-              <h1>{isMonthly ? "$25/Month" : "$250/Year"}</h1>
-              <p>Advanced analytics & insights</p>
-              <p>Team collaboration tools</p>
-              <p>Customizable budgeting tools</p>
-            <Button text="Get Started" onClick={() => navigate('/payment')} className='subscription-banner-section-card-btn' variant="white"/>
-        </div>    
+          <div className='home-section-subscription-card right'>
+            <h2>Premium plan</h2>
+            <h1>{isMonthly ? "$25/Month" : "$250/Year"}</h1>
+            <p>Advanced analytics & insights</p>
+            <p>Team collaboration tools</p>
+            <p>Customizable budgeting tools</p>
+            <Button text="Get Started" onClick={() => navigate('/payment')} className='subscription-banner-section-card-btn' variant="white" />
+          </div>
         </div>
       </section>
     </section>
   );
-};
+}
 
-export default Home
+
+export default Home;
